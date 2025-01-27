344ecf7f07e819f867d545f9913269ed9a613fdf
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-01-27 22:38:40 +0200

    Intergrated JS for dynamic timezones, real-time dates and times now live

---
+++ script.js
@@ -0,0 +1,24 @@
+function updateTime() {
+    const cities = [
+        {id: 'paris', timezoneOffset: '+02:00' },
+        {id: 'sydney', timezoneOffset: '+10:00' },
+        {id: 'new-york', timezoneOffset: '-05:00' },
+        {id: 'south-africa', timezoneOffset: '+02:00' },
+    ];
+
+    cities.forEach((city) => {
+        const now = moment();
+        const cityTime = now.utcOffset(city.timezoneOffset);
+
+        const formattedDate = cityTime.format('YYYY-MM-DD');
+        const formattedTime = cityTime.format('HH:mm:ss');
+        const amPm = cityTime.format('A');
+
+        const cityElement = document.getElementById(city.id);
+        cityElement.querySelector('.date').textContent = formattedDate;
+        cityElement.querySelector('.time').textContent = '${formattedTime} ${amPm}';
+    });
+}
+
+updateTime();
+setInterval(updateTime, 1000);
